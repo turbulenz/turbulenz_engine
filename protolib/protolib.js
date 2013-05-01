4e6e9fb6756891458009788bd5364b8cbfb3c7de
Ian Ballantyne <ianballantyne@turbulenz.biz>
2013-05-01 17:18:53 +0100

    Use jqueryextend to resolve the extend issue, seperate file, correctly referenced.

--- protolib/protolib.js
+++ protolib/protolib.js
@@ -23,6 +23,8 @@
 /*global VMathArrayConstructor: false*/
 /*global debug: false*/
 /*global Utilities: false*/
+/*global jQuery: false*/
+/*global jQueryExtend: false*/
 
 function Protolib(params)
 {
@@ -1709,155 +1711,22 @@ SpotLightWrapper.prototype =
     }
 };
 
-(function (window, undefined) {
-
-// Can't do this because several apps including ASP.NET trace
-// the stack via arguments.caller.callee and Firefox dies if
-// you try to trace through "use strict" call chains. (#13335)
-// Support: Firefox 18+
-//"use strict";
-    var
-    //core_version = "1.9.1",
-
-    class2type = {},
-
-    core_toString = class2type.toString,
-    core_hasOwn = class2type.hasOwnProperty,
-
-    jQuery = {
-
-        // Modified jQuery required for extend function
-
-        // See test/unit/core.js for details concerning isFunction.
-        // Since version 1.3, DOM methods and functions like alert
-        // aren't supported. They return false on IE (#2968).
-        isFunction: function (obj) {
-            return jQuery.type(obj) === "function";
-        },
-
-        isArray: Array.isArray || function (obj) {
-            return jQuery.type(obj) === "array";
-        },
-
-        isWindow: function (obj) {
-            return obj !== null && obj === obj.window;
-        },
-
-        isNumeric: function (obj) {
-            return !isNaN(parseFloat(obj)) && isFinite(obj);
-        },
-
-        type: function (obj) {
-            if (obj === null) {
-                return String(obj);
-            }
-            return typeof obj === "object" || typeof obj === "function" ?
-                class2type[core_toString.call(obj)] || "object" :
-                typeof obj;
-        },
-
-        isPlainObject: function (obj) {
-            // Must be an Object.
-            // Because of IE, we also have to check the presence of the constructor property.
-            // Make sure that DOM nodes and window objects don't pass through, as well
-            if (!obj || jQuery.type(obj) !== "object" || obj.nodeType || jQuery.isWindow(obj)) {
-                return false;
-            }
-
-            try {
-                // Not own constructor property must be Object
-                if (obj.constructor &&
-                    !core_hasOwn.call(obj, "constructor") &&
-                    !core_hasOwn.call(obj.constructor.prototype, "isPrototypeOf")) {
-                    return false;
-                }
-            } catch (e) {
-                // IE8,9 Will throw exceptions on certain host objects #9897
-                return false;
-            }
-
-            // Own properties are enumerated firstly, so to speed up,
-            // if last one is own, then all properties are own.
-
-            var key;
-            for (key in obj) {}
-
-            return key === undefined || core_hasOwn.call(obj, key);
-        },
-
-        isEmptyObject: function (obj) {
-            var name;
-            for (name in obj) {
-                return false;
-            }
-            return true;
+if (window.jQuery !== undefined)
+{
+    Protolib.extend = jQuery.extend;
+}
+else
+{
+    try
+    {
+        Protolib.extend = jQueryExtend;
+    }
+    catch (e)
+    {
+        var console = window.console;
+        if (console)
+        {
+            console.log("Error: jquery extend is missing. Have you included jqueryextend.js?");
         }
-    };
-
-
-    Protolib.extend = jQuery.extend = function extendFn() {
-
-        var src, copyIsArray, copy, name, options, clone,
-            target = arguments[0] || {},
-            i = 1,
-            length = arguments.length,
-            deep = false;
-
-        // Handle a deep copy situation
-        if (typeof target === "boolean") {
-            deep = target;
-            target = arguments[1] || {};
-            // skip the boolean and the target
-            i = 2;
-        }
-
-        // Handle case when target is a string or something (possible in deep copy)
-        if (typeof target !== "object" && !jQuery.isFunction(target)) {
-            target = {};
-        }
-
-        // extend jQuery itself if only one argument is passed
-        if (length === i) {
-            target = this;
-            i -= 1;
-        }
-
-        for (; i < length; i += 1) {
-            // Only deal with non-null/undefined values
-            if ((options = arguments[i]) !== null) {
-                // Extend the base object
-                for (name in options) {
-                    src = target[name];
-                    copy = options[name];
-
-                    // Prevent never-ending loop
-                    if (target === copy) {
-                        continue;
-                    }
-
-                    // Recurse if we're merging plain objects or arrays
-                    if (deep && copy && (jQuery.isPlainObject(copy) || (copyIsArray = jQuery.isArray(copy)))) {
-                        if (copyIsArray) {
-                            copyIsArray = false;
-                            clone = src && jQuery.isArray(src) ? src : [];
-
-                        } else {
-                            clone = src && jQuery.isPlainObject(src) ? src : {};
-                        }
-
-                        // Never move original objects, clone them
-                        target[name] = jQuery.extend(deep, clone, copy);
-
-                    // Don't bring in undefined values
-                    } else if (copy !== undefined) {
-                        target[name] = copy;
-                    }
-                }
-            }
-        }
-
-        // Return the modified object
-        return target;
-    };
-
-})(window);
+    }
+}
