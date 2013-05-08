58ba26edf9f74c9c5d7a690a6b55ae21bd0d3558
Ian Ballantyne <ianballantyne@turbulenz.biz>
2013-05-08 18:09:11 +0100

    Removed dependency on cached variables for clarity over efficiency. (Requested by AL). Renamed md -> mathDevice for clarity.

--- apps/protolibsampleapp/scripts/protolibsampleapp.js
+++ apps/protolibsampleapp/scripts/protolibsampleapp.js
@@ -15,39 +15,32 @@ Application.prototype =
     init: function initFn()
     {
         var protolib = this.protolib;
-        var md = protolib.getMathDevice();
+        var mathDevice = protolib.getMathDevice();
 
         //Vars
         this.frameCount = 0;
-
-        this.origin = md.v3Build(0, 0, 0);
-        this.white = md.v3Build(1, 1, 1);
-        this.red = md.v3Build(1, 0, 0);
-        this.unitX = md.v3BuildXAxis();
-        this.unitY = md.v3BuildYAxis();
-        this.unitZ = md.v3BuildZAxis();
         this.degToRad = Math.PI / 180;
 
-        this.cameraPosition = md.v3Build(0, 0, 0);
-        this.cameraDirection = md.v3Build(0, 0, 0);
+        this.cameraPosition = mathDevice.v3Build(0, 0, 0);
+        this.cameraDirection = mathDevice.v3Build(0, 0, 0);
 
         this.cameraRotation = 0;
         this.cameraRadius = 3;
-        this.cameraFocus = md.v3Build(0, 1, 0);
+        this.cameraFocus = mathDevice.v3Build(0, 1, 0);
 
-        this.lightPosition = md.v3Build(1, 3, -1);
-        this.lightDirection = md.v3Build(-1, -3, 1);
-        this.lightColor = md.v3Build(1, 1, 1);
+        this.lightPosition = mathDevice.v3Build(1, 3, -1);
+        this.lightDirection = mathDevice.v3Build(-1, -3, 1);
+        this.lightColor = mathDevice.v3Build(1, 1, 1);
         this.lightRotation = 0;
 
-        this.spinningLogoRotationMatrix = md.m43BuildIdentity();
+        this.spinningLogoRotationMatrix = mathDevice.m43BuildIdentity();
 
         //Camera
         protolib.setCameraFOV(90 * this.degToRad, 90 * this.degToRad);
         protolib.setNearFarPlanes(0.01, 10);
 
         //Lights
-        var bgColor = md.v3Build(0.5, 0.5, 0.5);
+        var bgColor = mathDevice.v3Build(0.5, 0.5, 0.5);
         protolib.setClearColor(bgColor);
         protolib.setAmbientLightColor(bgColor);
 
@@ -62,14 +55,14 @@ Application.prototype =
         //Meshes
         this.spinningLogo = protolib.loadMesh({
             mesh: 'models/tz_logo.dae',
-            v3Size: md.v3Build(0.5, 0.5, 0.5),
-            v3Position: md.v3Build(0, 1, 0)
+            v3Size: mathDevice.v3Build(0.5, 0.5, 0.5),
+            v3Position: mathDevice.v3Build(0, 1, 0)
         });
 
         this.whiteFloor = protolib.loadMesh({
             mesh: 'models/white_cube.dae',
-            v3Size: md.v3Build(50, 0.1, 50),
-            v3Position: md.v3Build(0, -0.06, 0)
+            v3Size: mathDevice.v3Build(50, 0.1, 50),
+            v3Position: mathDevice.v3Build(0, -0.06, 0)
         });
 
         //Sound
@@ -114,7 +107,7 @@ Application.prototype =
         this.turbulenzText = {
             text: 'Powered by Turbulenz',
             position: [180, protolib.height - 100],
-            v3Color: this.white,
+            v3Color: [1.0, 1.0, 1.0],
             scale: 1
         };
         this.turbulenzTextScaleSliderID = protolib.addWatchVariable({
@@ -135,7 +128,7 @@ Application.prototype =
     {
         var protolib = this.protolib;
         var utils = protolib.utils;
-        var md = protolib.getMathDevice();
+        var mathDevice = protolib.getMathDevice();
 
         if (protolib.beginFrame())
         {
@@ -178,7 +171,7 @@ Application.prototype =
             {
                 protolib.draw2DSprite({
                     texture: 'textures/cross.png',
-                    v3Color: this.red,
+                    v3Color: [1.0, 0.0, 0.0],
                     position: [protolib.width - 100, protolib.height - 100],
                     width: 50,
                     height: 50
@@ -197,7 +190,7 @@ Application.prototype =
                 v3Color: this.lightColor
             });
 
-            md.m43SetAxisRotation(this.spinningLogoRotationMatrix, this.unitY, this.frameCount / 250);
+            mathDevice.m43SetAxisRotation(this.spinningLogoRotationMatrix, mathDevice.v3BuildYAxis(), this.frameCount / 250);
             this.spinningLogo.setRotationMatrix(this.spinningLogoRotationMatrix);
 
             //Camera controller: Rotate camera around y axis.
@@ -214,10 +207,10 @@ Application.prototype =
 
             var x = this.cameraRadius * Math.sin(this.cameraRotation);
             var z = this.cameraRadius * Math.cos(this.cameraRotation);
-            md.v3Build(x, 2.5, z, this.cameraPosition);
+            mathDevice.v3Build(x, 2.5, z, this.cameraPosition);
             protolib.setCameraPosition(this.cameraPosition);
 
-            md.v3Sub(this.cameraFocus, this.cameraPosition, this.cameraDirection);
+            mathDevice.v3Sub(this.cameraFocus, this.cameraPosition, this.cameraDirection);
             protolib.setCameraDirection(this.cameraDirection);
 
             //Rotate light around y axis.
@@ -229,15 +222,15 @@ Application.prototype =
             {
                 this.lightRotation += 0.05;
             }
-            md.v3Build(Math.cos(this.lightRotation), 3, Math.sin(this.lightRotation), this.lightPosition);
+            mathDevice.v3Build(Math.cos(this.lightRotation), 3, Math.sin(this.lightRotation), this.lightPosition);
             this.spotLight.setPosition(this.lightPosition);
-            md.v3ScalarMul(this.lightPosition, -1, this.lightDirection);
+            mathDevice.v3ScalarMul(this.lightPosition, -1, this.lightDirection);
             this.spotLight.setDirection(this.lightDirection);
 
             //3D Axis
-            protolib.draw3DLine({pos1: this.origin, pos2: this.unitX, v3Color: this.unitX});
-            protolib.draw3DLine({pos1: this.origin, pos2: this.unitY, v3Color: this.unitY});
-            protolib.draw3DLine({pos1: this.origin, pos2: this.unitZ, v3Color: this.unitZ});
+            protolib.draw3DLine({pos1: [0, 0, 0], pos2: [1, 0, 0], v3Color: [1, 0, 0]});
+            protolib.draw3DLine({pos1: [0, 0, 0], pos2: [0, 1, 0], v3Color: [0, 1, 0]});
+            protolib.draw3DLine({pos1: [0, 0, 0], pos2: [0, 0, 1], v3Color: [0, 0, 1]});
 
             protolib.draw2DSprite({
                 texture: "textures/cursor.png",
