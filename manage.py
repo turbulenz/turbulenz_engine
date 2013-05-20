175dc1f45eeb248805d6f4b651194a9eca41ad81
Blake Maltby <blakemaltby@turbulenz.biz>
2013-05-20 12:58:52 +0100

    Update required versions of turbulenz_local and turbulenz_tools

--- manage.py
+++ manage.py
@@ -60,8 +60,8 @@ def command_env():
     _easy_install('docutils>=0.9.1')
     _easy_install('Sphinx>=1.1.3')
 
-    _easy_install('turbulenz_tools>=1.0')
-    _easy_install('turbulenz_local>=1.0')
+    _easy_install('turbulenz_tools>=1.0.1')
+    _easy_install('turbulenz_local>=1.0.1')
 
     cmd = [os.path.join(env_bin, 'python'), os.path.join('scripts', 'install_nodejs.py'), '--typescript']
     if not TURBULENZOS in [ 'linux32', 'linux64' ]:
