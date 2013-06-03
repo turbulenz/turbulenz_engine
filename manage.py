066e2b5f283a5e7cdd20a0ef5407b60d23d4660a
Blake Maltby <blakemaltby@turbulenz.biz>
2013-06-03 16:22:39 +0100

    Update required version of turbulenz_local package

--- manage.py
+++ manage.py
@@ -67,7 +67,7 @@ def command_env():
     _easy_install('Sphinx>=1.1.3')
 
     _easy_install('turbulenz_tools>=1.0.1')
-    _easy_install('turbulenz_local>=1.0.2')
+    _easy_install('turbulenz_local>=1.1')
 
     cmd = [os.path.join(env_bin, 'python'),
            os.path.join(TURBULENZROOT, 'scripts', 'install_nodejs.py'),
