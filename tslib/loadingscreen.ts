2ba2e127f97d352b18c74940f1ca17dc26c8b53c
David Galeano <davidgaleano@turbulenz.biz>
2013-07-02 17:00:30 +0100

    Updated embedded shader code.

--- tslib/loadingscreen.ts
+++ tslib/loadingscreen.ts
@@ -321,8 +321,8 @@ class LoadingScreen
                     {
                         "MinFilter": 9729,
                         "MagFilter": 9729,
-                        "WrapS": 10496,
-                        "WrapT": 10496
+                        "WrapS": 33071,
+                        "WrapT": 33071
                     }
                 },
                 "parameters":
@@ -386,22 +386,22 @@ class LoadingScreen
                     "fp_texture":
                     {
                         "type": "fragment",
-                        "code": "#ifdef GL_ES\nprecision mediump float;precision mediump int;\n#endif\nvarying vec4 tz_TexCoord[1];float _TMP10;float _b0015;uniform float alpha;uniform sampler2D diffuse;void main()\n{vec4 _textureColor;_textureColor=texture2D(diffuse,tz_TexCoord[0].xy);_b0015=min(1.0,alpha);_TMP10=max(0.0,_b0015);_textureColor.w=_textureColor.w*_TMP10*_TMP10*(3.0-2.0*_TMP10);gl_FragColor=_textureColor;}"
+                        "code": "#ifdef GL_ES\n#define TZ_LOWP lowp\nprecision mediump float;\nprecision mediump int;\n#else\n#define TZ_LOWP\n#endif\nvarying vec4 tz_TexCoord[1];\nfloat _TMP0;float _TMP1;float _TMP12;uniform float alpha;uniform sampler2D diffuse;void main()\n{vec4 _textureColor;_textureColor=texture2D(diffuse,tz_TexCoord[0].xy);_TMP1=min(1.0,alpha);_TMP12=max(0.0,_TMP1);_TMP0=_TMP12*_TMP12*(3.0-2.0*_TMP12);_textureColor.w=_textureColor.w*_TMP0;gl_FragColor=_textureColor;}"
                     },
                     "vp_texture":
                     {
                         "type": "vertex",
-                        "code": "#ifdef GL_ES\nprecision mediump float;precision mediump int;\n#endif\nvarying vec4 tz_TexCoord[1];attribute vec4 ATTR0;attribute vec4 ATTR8;\nuniform vec4 clipSpace;void main()\n{vec4 _position;_position=ATTR0;_position.xy=ATTR0.xy*clipSpace.xy+clipSpace.zw;tz_TexCoord[0].xy=ATTR8.xy;gl_Position=_position;}"
+                        "code": "#ifdef GL_ES\n#define TZ_LOWP lowp\nprecision mediump float;\nprecision mediump int;\n#else\n#define TZ_LOWP\n#endif\nvarying vec4 tz_TexCoord[1];attribute vec4 ATTR0;attribute vec4 ATTR8;\nvec4 _OutPosition1;vec2 _OutUV1;uniform vec4 clipSpace;void main()\n{_OutPosition1.xy=ATTR0.xy*clipSpace.xy+clipSpace.zw;_OutPosition1.zw=ATTR0.zw;_OutUV1=ATTR8.xy;tz_TexCoord[0].xy=ATTR8.xy;gl_Position=_OutPosition1;}"
                     },
                     "fp_background":
                     {
                         "type": "fragment",
-                        "code": "#ifdef GL_ES\nprecision mediump float;precision mediump int;\n#endif\nvec4 _ret_0;float _TMP9;float _b0014;uniform vec4 color;void main()\n{_b0014=min(1.0,color.w);_TMP9=max(0.0,_b0014);_ret_0=vec4(color.x,color.y,color.z,_TMP9*_TMP9*(3.0-2.0*_TMP9));gl_FragColor=_ret_0;}"
+                        "code": "#ifdef GL_ES\n#define TZ_LOWP lowp\nprecision mediump float;\nprecision mediump int;\n#else\n#define TZ_LOWP\n#endif\nvec4 _ret_0;float _TMP0;float _TMP1;float _TMP11;uniform vec4 color;void main()\n{_TMP1=min(1.0,color.w);_TMP11=max(0.0,_TMP1);_TMP0=_TMP11*_TMP11*(3.0-2.0*_TMP11);_ret_0=vec4(color.x,color.y,color.z,_TMP0);gl_FragColor=_ret_0;}"
                     },
                     "vp_background":
                     {
                         "type": "vertex",
-                        "code": "#ifdef GL_ES\nprecision mediump float;precision mediump int;\n#endif\nattribute vec4 ATTR0;\nvoid main()\n{gl_Position=ATTR0;}"
+                        "code": "#ifdef GL_ES\n#define TZ_LOWP lowp\nprecision mediump float;\nprecision mediump int;\n#else\n#define TZ_LOWP\n#endif\nattribute vec4 ATTR0;\nvoid main()\n{gl_Position=ATTR0;}"
                     }
                 }
             };
