b1dc83e51342a98960ac84cdf5fd2c0a6c61d9d9
Luca Deltdesco <lucadeltodesco@turbulenz.com>
2013-10-02 14:24:11 +0100

    Move texturepacking app into engines/onlinetexturepacker.

---
+++ apps/particlesystem/onlinetexturepacker/templates/onlinetexturepacker.js
@@ -0,0 +1,8 @@
+/*{# Copyright (c) 2010-2012 Turbulenz Limited #}*/
+
+/*{{ javascript("scripts/onlinetexturepacker.js") }}*/
+
+/*{{ javascript("jslib/draw2d.js") }}*/
+/*{{ javascript("jslib/physics2ddevice.js") }}*/
+/*{{ javascript("jslib/physics2ddebugdraw.js") }}*/
+/*{{ javascript("jslib/particlesystem.js") }}*/
