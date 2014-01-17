ef56b903e4e3115309053aee42bff1b8511f49ce
Ian Ballantyne <ianballantyne@turbulenz.biz>
2014-01-17 12:57:27 +0000

    Moved additional particlesystem apps to main repository

--- apps/particlesystem/packtextures/templates/packtextures.js
+++
@@ -1,8 +0,0 @@
-/*{# Copyright (c) 2010-2012 Turbulenz Limited #}*/
-
-/*{{ javascript("scripts/packtextures.js") }}*/
-
-/*{{ javascript("jslib/draw2d.js") }}*/
-/*{{ javascript("jslib/physics2ddevice.js") }}*/
-/*{{ javascript("jslib/physics2ddebugdraw.js") }}*/
-/*{{ javascript("jslib/particlesystem.js") }}*/
